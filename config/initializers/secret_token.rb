5c6090b276509abe68afcb0f8a384fae0ee76665
jaipratik <jp_ca@live.com>
2011-11-10 20:58:22 -0800

    changed README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapiApp::Application.config.secret_token = '90ec995360db2614ad1b24dd3d0353e41b6a91f8238d6d2d4d0c2d4a7887f008aee66f1cd5cd80952eb7708aa43c70f3642e51be33aba4401b67d0466adbf738'
