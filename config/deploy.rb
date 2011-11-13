96036e200048bb6c6178b22dc74eebebe089951e
JP-R <jayparteek@JPs-Pro.local>
2011-11-13 12:39:20 -0800

    run db migrate

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,23 @@
 set :application, "capi_app"
-set :repository,  "git@github.com:jaipratik/capi_app.git"
-
-set :scm, :git
-
-set :user, "jp"
-set :use_sudo, false
 set :deploy_to, "/home/jp/sites/capi_app"
-
 
 role :web, "192.168.2.129"                          # Your HTTP server, Apache/etc
 role :app, "192.168.2.129"                          # This may be the same as your `Web` server
 role :db,  "192.168.2.129", :primary => true # This is where Rails migrations will run
-  
+
+
+default_run_options[:pty] =  true
+set :repository,  "git://git@github.com:jaipratik/capi_app.git"
+set :scm, :git
+set :branch, "master"
+
+
+set :user, "jp"            #if error use whats shown in podcast
+set :admin_runner, "jp"
+
+#set :use_sudo, false   #if error delete this
+
+
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -24,4 +30,3 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
-
