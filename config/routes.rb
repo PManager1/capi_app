359787ea009bb21fac3ba2af79d316d53358afcb
jaipratik <jp_ca@live.com>
2011-11-24 18:06:28 -0800

    root route to bandpage

--- config/routes.rb
+++ config/routes.rb
@@ -52,8 +52,8 @@ CapiApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'posts#index'
-  # root :to => 'bandpage#index'
+  #root :to => 'posts#index'
+   root :to => 'bandpage#index'
 
 
   # See how all your routes lay out with "rake routes"
