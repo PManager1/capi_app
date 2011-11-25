dfc67332692617e5417cc8019b7b391c783a0516
jaipratik <jp_ca@live.com>
2011-11-24 18:35:49 -0800

    added css and js files

---
+++ app/assets/javascripts/includes/video_gallery.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	
+	$('a.videoLink').each(function(){
+		
+		var thumbnailFilePath = 'video/' + $(this).attr('videofile') + '.jpg';  // this will fetch the image file from folder by using the same video name + .jpg
+  		
+		var videoCaption = $(this).attr('videocaption');
+		
+		})
+	
+	
+	});
